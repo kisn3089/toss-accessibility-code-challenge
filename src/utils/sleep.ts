f7b85f03450692fc76d56fd4ab0efeec86a2213b
Stems <kisn3089@naver.com>
2025-09-22 21:06:03 +0900

    fix: sleep 주석 수정

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,5 +1,5 @@
 /**
- * @param callback 지연 후 호출할 함수
+ * @param callback 지연 호출할 함수
  * @param delay 단위: ms
  * @example function logging(str: string) { console.log(str) }
  * sleep(() => logging("time"), 1500)
