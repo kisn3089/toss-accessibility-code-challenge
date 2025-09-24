c286d9f9d41510b749378536f92c6f7267543d41
Stems <kisn3089@naver.com>
2025-09-24 20:07:26 +0900

    fix: validator util 함수 메서드 -> 화살표 함수로 변경

--- src/utils/validation/validator.ts
+++ src/utils/validation/validator.ts
@@ -1,23 +1,38 @@
 import { regex } from "./regex";
 
 export const formValidator = {
-  notEmpty(value: string = ""): boolean {
+  notEmpty: (value: string = ""): boolean => {
     return value?.trim() !== "";
   },
 
-  emailRegex(value: string = ""): boolean {
+  emailRegex: (value: string = ""): boolean => {
     return regex.email.test(value);
   },
 
-  minLength(value: string = "", min: number): boolean {
+  minLength: (value: string = "", min: number): boolean => {
     return value.trim().length >= min;
   },
 
-  maxLength(value: string = "", max: number): boolean {
+  maxLength: (value: string = "", max: number): boolean => {
     return value.trim().length <= max;
   },
 
-  lengthInRange(value: string = "", min: number, max: number): boolean {
-    return this.minLength(value, min) && this.maxLength(value, max);
+  lengthInRange: (value: string = "", min: number, max: number): boolean => {
+    return (
+      formValidator.minLength(value, min) && formValidator.maxLength(value, max)
+    );
+  },
+
+  nameValidator: (name: string = "") => {
+    return (
+      formValidator.notEmpty(name) && formValidator.lengthInRange(name, 2, 20)
+    );
+  },
+
+  emailValidator: (email: string = "") => {
+    return (
+      formValidator.emailRegex(email) &&
+      formValidator.lengthInRange(email, 5, 50)
+    );
   },
 };
