d1afc27493c0a6cee0bd736892d6420f1566191b
Stems <kisn3089@naver.com>
2025-09-24 20:21:34 +0900

    feat: 모달 오픈되었다면 뒤에 있는 HTML 비활성화하여 tab 이동 효율 향상

--- src/pages/modalFormPage/ModalFormPage.tsx
+++ src/pages/modalFormPage/ModalFormPage.tsx
@@ -5,15 +5,17 @@ import Button from "../../components/atom/button/Button";
 import type { ModalFormData } from "../../components/modalForm/formBody/FormBody.type";
 
 export const ModalFormPage = () => {
-  const { push } = useModal();
+  const { push, getModals } = useModal();
 
   const addModal = async () => {
     const pushedResult = await push<ModalFormData>(<ModalForm />);
     console.log("pushedResult: ", pushedResult);
   };
 
+  const isOpenedModal = getModals().length !== 0;
+
   return (
-    <Center>
+    <Center inert={isOpenedModal}>
       <Button tabIndex={1} onClick={addModal}>
         {"🚀 신청 폼 작성하기"}
       </Button>
